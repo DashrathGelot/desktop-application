59f92a8111cefd0712b27024242816be19b46fbb
DashrathGelot <gelotdashrath9731@gmail.com>
2020-04-27 21:23:18 +0530

    added batery alarm application

---
+++ Battery/Battery/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Battery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(12, 81);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(124, 81);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(75, 23);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 145);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.Start);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Battery Alarm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Battery/Battery/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Media;
+
+namespace Battery
+{
+    public partial class Form1 : Form
+    {
+        Timer timer1;
+        SoundPlayer player = new SoundPlayer();
+        double l = 100,B;
+        public Form1()
+        {
+            InitializeComponent();
+        }    
+        private void Start_Click(object sender, EventArgs e)
+        {
+            String batterystatus;
+            PowerStatus pwr = SystemInformation.PowerStatus;
+            batterystatus = SystemInformation.PowerStatus.BatteryChargeStatus.ToString();
+            String batterylife;
+            batterylife = SystemInformation.PowerStatus.BatteryLifePercent.ToString();
+            B = Convert.ToDouble(batterylife);
+            B = B * 100;
+            Console.WriteLine(B);
+            label1.Text = "Battery level : " + B.ToString() + "%  Battery Status: " + batterystatus;
+            if (B == l)
+            {
+                SoundPlayer player = new SoundPlayer();
+                player.SoundLocation = "C:\\example\\Sample1.wav";
+                player.PlayLooping();
+                timer1.Enabled = false;
+                //player.Play();
+            }  
+        }
+
+        public void InitTimer()
+        {
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(Start_Click);
+            timer1.Interval = 200;
+            timer1.Start();
+            
+        }
+        private void stop_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitTimer();
+        }
+    }
+}
